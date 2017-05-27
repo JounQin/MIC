0a9f6b8ce9e2a71ad14843fa62071fa772260312
JounQin <admin@1stg.me>
2017-05-27 22:32:27 +0800

    hack Vue.prototype $http getter for HMR

--- src/plugins/http.js
+++ src/plugins/http.js
@@ -1,14 +1,18 @@
 import Vue from 'vue'
 import axios from 'axios'
 
+const {prototype: proto} = Vue
+
 axios.defaults.baseURL = (__SERVER__ ? INNER_SERVER : SERVER_PREFIX) + 'api'
 
-if (__SERVER__) {
-  Vue.mixin({
-    beforeCreate() {
-      Object.defineProperty(this, '$http', {value: this.$vnode && this.$ssrContext.axios})
+if (!__DEV__ || !proto.$httpInstalled) {
+  Object.defineProperty(proto, '$http', __SERVER__ ? {
+    get() {
+      return this.$vnode.ssrContext.axios
     }
-  })
-} else {
-  Object.defineProperty(Vue.prototype, '$http', {value: axios})
+  } : {value: axios})
+
+  if (__DEV__) {
+    proto.$httpInstalled = true
+  }
 }
